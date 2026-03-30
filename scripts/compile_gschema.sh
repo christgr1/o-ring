01a768b56f433c8a19a89d643eb58fa200450ccb
Grigorios Christodoulou <28005073+christgr1@users.noreply.github.com>
2026-03-30 20:14:11 +0200

    add useful VS Code tasks

--- scripts/compile_gschema.sh
+++ scripts/compile_gschema.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Compile gschemas in the current directory
-glib-compile-schemas ../o-ring@christgr1.github.io/schemas/
+glib-compile-schemas o-ring@christgr1.github.io/schemas/
 
 # Check if compilation was successful
 if [ $? -eq 0 ]; then
